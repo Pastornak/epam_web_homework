5653912f09c87dc36a94534221765d7e28ef5aa8
Yurii Pasternak <yurapaster@gmail.com>
2018-09-15 03:22:21 +0300

    Improved code and list style

--- lab_5(todo-list)/script.js
+++ lab_5(todo-list)/script.js
@@ -2,12 +2,16 @@ var Tasks = (function(){
     var tasks = new Map();
     var index = 1;
     var addTask = function(str){
-        tasks.set(index, str);
+        var taskObject = {
+            description: str,
+            finished: false
+        };
+        tasks.set(index, taskObject);
         index++;
     };
     var deleteTaskByString = function(str){
         for(var [key, value] of tasks){
-            if(value == str){
+            if(value.description == str){
                 tasks.delete(key);
             }
         }
@@ -17,6 +21,11 @@ var Tasks = (function(){
     };
 
     return{
+        toogleIsFinished: function(taskID){
+            var taskObject = tasks.get(taskID);
+            taskObject.finished = !taskObject.finished;
+            tasks.set(taskID, taskObject);
+        },
         get: function(){
             return tasks;
         },
@@ -40,83 +49,43 @@ function addTaskToList(){
     }
 }
 
-function updateList(taskID){
-    // Delete this element from UI if ID is passed
-    if(taskID){
-        var listElement = document.getElementById(taskID);
-        listElement.parentNode.removeChild(listElement);
-    }
-    // Otherwise add new element to the end of the list
-    else{
-        var tasks = Tasks.get();
-        if(tasks.size <= 0){
-            return;
+function updateList(condition){
+    var tasks = Tasks.get();
+    // if(condition !== undefined){
+    //     tasks = filterTasks(Boolean(condition));
+    // } else{
+    //     tasks = Tasks.get();
+    // }
+    var list = document.getElementById("todo-list");
+    list.innerHTML = "";
+    for(var [key, value] of tasks){
+        if(value.finished){
+            list.innerHTML += "<li id='" + key + "'><input type='checkbox' class='checkbox-button' onclick='toogleTaskAsFinished(" + key  + ")' checked></button>"+ "<span class='big-text'><del>"
+                + value.description + "</del></span>" + "<button type='button' class='close-button' onclick='deleteTaskFromList(" + key + ")'></button></li>\n";
+        } else{
+            list.innerHTML += "<li id='" + key + "'><input type='checkbox' class='checkbox-button' onclick='toogleTaskAsFinished(" + key  + ")'></button>" + "<span class='big-text'>" 
+                + value.description + "</span>" + "<button type='button' class='close-button' onclick='deleteTaskFromList(" + key + ")'></button></li>\n";
         }
-        var size = tasks.size - 1;
-        var newTaskID;
-        var newTaskDef;
-        var tempVariable = 0;
-        for(var [key, value] of tasks){
-            if(tempVariable == size){
-                newTaskID = key;
-                newTaskDef = value;
-                break;
-            }
-            tempVariable++;
-        }
-        var list = document.getElementById("todo-list");
-
-        var checkboxElement = document.createElement("input");
-        checkboxElement.setAttribute("type", "checkbox");
-        checkboxElement.setAttribute("class", "checkbox-button");
-        checkboxElement.setAttribute("onclick", "toogleTaskAsFinished(" + newTaskID  + ")");
-        
-        var spanElement = document.createElement("span");
-        spanElement.setAttribute("class", "big-text");
-        var spanContent = document.createTextNode(newTaskDef);
-        spanElement.appendChild(spanContent);
-        
-        var buttonElement = document.createElement("button", {type: "button", class: "close-button", onclick: "deleteTaskFromList(" + newTaskID + ")"});
-        buttonElement.setAttribute("type", "button");
-        buttonElement.setAttribute("class", "close-button");
-        buttonElement.setAttribute("onclick", "deleteTaskFromList(" + newTaskID + ")");
-
-        var listElement = document.createElement("li");
-        listElement.setAttribute("id", newTaskID);
-        listElement.appendChild(checkboxElement);
-        listElement.appendChild(spanElement);
-        listElement.appendChild(buttonElement);
-        list.appendChild(listElement);
-        // list.innerHTML += "<li id='" + newTaskID + "'><input type='checkbox' class='checkbox-button' onclick='toogleTaskAsFinished(" + newTaskID  + ")'></button><span class='big-text'>" 
-        //     + newTaskDef + "</span><button type='button' class='close-button' onclick='deleteTaskFromList(" + newTaskID + ")'></button></li>\n";
     }
 }
 
+// function filterTasks(condition){
+//     var result = new Map();
+//     var tasks = Tasks.get();
+//     for(var [key, value] of tasks){
+//         if(value.finished == condition){
+//             result.set(key, value);
+//         }
+//     }
+//     return result;
+// }
+
 function toogleTaskAsFinished(taskID){
-    var listElement = document.getElementById(taskID);
-    console.log(listElement.innerHTML);
-    var inputElement = listElement.getElementsByTagName("input")[0];
-    var spanElement = listElement.getElementsByTagName("span")[0];
-    var delElement = spanElement.getElementsByTagName("del")[0];
-    if(delElement){
-        spanElement.innerHTML = delElement.innerHTML;
-        inputElement.checked = false;
-    } else{
-        spanElement.innerHTML = "<del>" + spanElement.innerHTML + "</del>";
-        inputElement.checked = true;
-    }
+    Tasks.toogleIsFinished(taskID);
+    updateList();
 }
 
 function deleteTaskFromList(taskID){
-    var listElement = document.getElementById(taskID);
-    var spanElement = listElement.getElementsByTagName("span")[0];
-    var delElement = spanElement.getElementsByTagName("del")[0];
-    var value;
-    if(delElement){
-        value = delElement.innerHTML;
-    } else{
-        value = spanElement.innerHTML;
-    }
     Tasks.deleteByID(taskID);
-    updateList(taskID);
+    updateList();
 }
